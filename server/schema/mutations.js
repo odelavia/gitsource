e5570d61b8b0c62cdc14beadc55f5e4311601f92
odelavia <odelavia@gmail.com>
2018-08-26 22:26:46 -0400

    refactored and modified rootquerytype to show user when signed in

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -32,9 +32,6 @@ const mutation = new GraphQLObjectType({
     logout: {
       type: UserType,
       resolve(parentValue, args, req) {
-        // const { user } = req;
-        // req.logout();
-        // return user;
         return AuthService.logout({ req })
       }
     }
