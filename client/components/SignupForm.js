ea7f5d5313608fd3b4f582385f6220b25d6585da
odelavia <odelavia@gmail.com>
2018-08-27 00:40:08 -0400

    set up Signup form, query, error handling. Still needs style finessing

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo';
-import mutation from '../mutations/Login';
+import mutation from '../mutations/Signup';
 import query from '../queries/CurrentUser';
 
-class LoginForm extends Component {
+class SignupForm extends Component {
   constructor(props) {
     super(props);
 
@@ -27,7 +27,7 @@ class LoginForm extends Component {
   render() {
     return(
       <div>
-        <h3>Login</h3>
+        <h3>Sign Up</h3>
         <AuthForm
           errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)} />
@@ -36,4 +36,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(mutation)(SignupForm);
